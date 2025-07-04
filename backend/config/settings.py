ebeb2d516e81a71b581b49f0c5dca8f9e4564081
Yoanly Perez <98776473+yoanlypa@users.noreply.github.com>
2025-07-04 22:06:01 +0200

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -157,9 +157,10 @@ LOGIN_URL = "pedidos:acceso"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CSRF_TRUSTED_ORIGINS = [r"https://.*\.up\.railway\.app"]
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://innovations-tours.up.railway.app",
+    "https://*.up.railway.app",   
+]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
