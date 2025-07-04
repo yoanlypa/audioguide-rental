051924694a275da1add8de6f6c7fc065391f5ebb
yoanlypa <pyoanly@gmail.com>
2025-07-04 22:53:26 +0200

    Fix: import re_path for Swagger routes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -53,11 +53,8 @@ urlpatterns = [
     path('api/auth/', include('dj_rest_auth.urls')),
     path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
     
-        # Swagger UI
-    re_path(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
-    # Redoc (opcional)
-    re_path(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/",   schema_view.with_ui("redoc",   cache_timeout=0), name="schema-redoc"),
  
     # endpoints de pedidos
     path('api/', include(router.urls)),
