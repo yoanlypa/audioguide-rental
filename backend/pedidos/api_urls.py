87b5b1fdc1b10399d243f33fcaf6874d1fdea0fe
Yoanly Perez <98776473+yoanlypa@users.noreply.github.com>
2025-07-07 12:57:15 +0200

    Update api_urls.py

--- backend/pedidos/api_urls.py
+++ backend/pedidos/api_urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PedidoViewSet
+from .views import PedidoViewSet, BulkPedidos
 
 router = DefaultRouter()
 router.register(r'pedidos', PedidoViewSet, basename='pedido')
