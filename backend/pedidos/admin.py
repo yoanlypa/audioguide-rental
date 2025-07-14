321fb7fdb6eceb2afdf9e1f769d5873e1c353278
Yoanly Perez <98776473+yoanlypa@users.noreply.github.com>
2025-07-14 19:35:20 +0200

    Update admin.py

--- backend/pedidos/admin.py
+++ backend/pedidos/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import  Empresa
-from .models import Pedido, CustomUser
+from .models import  Empresa, Pedido, CustomUser, PedidoCrucero 
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
@@ -9,7 +8,13 @@ class CustomUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
         ('Información adicional', {'fields': ('empresa',)}),
     )
+class PedidoCruceroAdmin(admin.ModelAdmin):
+    list_display  = ("service_date", "ship", "sign", "excursion", "pax")
+    list_filter   = ("service_date", "ship")
+    search_fields = ("sign", "excursion", "language")
+    date_hierarchy = "service_date"
 
 admin.site.register(Pedido)
+@admin.register(PedidoCrucero)
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Empresa)
