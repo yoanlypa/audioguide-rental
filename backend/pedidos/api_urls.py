c1949cef948c7fcb7b0b9b8893f5996d4e7b4f45
yoanlypa <pyoanly@gmail.com>
2025-07-16 03:10:25 +0200

    fix migrations8

--- backend/pedidos/api_urls.py
+++ backend/pedidos/api_urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PedidoViewSet, BulkPedidos, CruceroBulkView
+from .views import PedidoViewSet, BulkPedidos, CruceroBulkView, PedidoCruceroListView
 
 router = DefaultRouter()
 router.register(r'pedidos', PedidoViewSet, basename='pedido')
@@ -8,5 +8,7 @@ router.register(r'pedidos', PedidoViewSet, basename='pedido')
 urlpatterns = [
     path('', include(router.urls)),
     path("pedidos/bulk/", BulkPedidos.as_view()),
+    path('api/pedidos/cruceros/',     PedidoCruceroListView.as_view()),
+
     path("pedidos/cruceros/bulk/", CruceroBulkView.as_view()),
 ]
