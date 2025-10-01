b2facae90070090e3260e55e55cd046214be6ebe
Yoanly Perez <98776473+yoanlypa@users.noreply.github.com>
2025-10-01 12:12:43 +0200

    Update views.py

--- backend/pedidos/views.py
+++ backend/pedidos/views.py
@@ -10,6 +10,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import EmailTokenObtainPairSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import action, api_view, permission_classes
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from django.db import transaction
