2be6d6d0109f45f2f52cfa3d3f20569a18b3d223
Yoanly Perez <98776473+yoanlypa@users.noreply.github.com>
2025-10-01 12:08:58 +0200

    Update api_urls.py

--- backend/pedidos/api_urls.py
+++ backend/pedidos/api_urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PedidoViewSet, BulkPedidos, CruceroBulkView
+from .views import PedidoViewSet, BulkPedidos, CruceroBulkView, PedidoOpsViewSet, me_view
 
 router = DefaultRouter()
-router.register(r'pedidos', PedidoViewSet, basename='pedido')
+router.register(r"ops/pedidos", PedidoOpsViewSet, basename="ops-pedidos")
 
 urlpatterns = [
     path('', include(router.urls)),
     path("pedidos/bulk/", BulkPedidos.as_view()),
-
+    path("me/", me_view, name="me"),
     path("pedidos/cruceros/bulk/", CruceroBulkView.as_view()),
 ]
